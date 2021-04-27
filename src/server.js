5bc92f330a128ae782f2c1df84b254556835b0ca
kyu <kyu.fastai@gmail.com>
2021-04-27 04:25:55 -0500

    server responses

--- src/server.js
+++ src/server.js
@@ -4,8 +4,19 @@ const PORT = 4000;
 
 const app = express();
 
-app.get("/", () => console.log("Somebody is trying to go home."));
+const handleHome = (request, response) => {
+    // return response.end(); // ending the request (one of many ways to end)
+    return response.send("I still love you."); // another way to end the request (with a sweet msg)
+}
+
+const handleLogin = (request, response) => {
+    return response.send("Login here.");
+}
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
+
