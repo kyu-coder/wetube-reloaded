81e58fe47f2b952048ddd8bc6919362d0084020d
kyu <kyu.fastai@gmail.com>
2021-04-27 04:10:07 -0500

    get request

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const PORT = 4000;
 
 const app = express();
 
+app.get("/", () => console.log("Somebody is trying to go home."));
+
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
