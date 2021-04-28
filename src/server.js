79b5a6fd63b71dcbd9433905ab55a8ce91396aba
kyu <kyu.fastai@gmail.com>
2021-04-27 23:12:55 -0500

    playing around with response.send

--- src/server.js
+++ src/server.js
@@ -6,11 +6,11 @@ const app = express();
 
 const handleHome = (request, response) => {
     // return response.end(); // ending the request (one of many ways to end)
-    return response.send("I still love you."); // another way to end the request (with a sweet msg)
+    return response.send("<h1> I still love you. </h1>"); // another way to end the request (with a sweet msg)
 }
 
 const handleLogin = (request, response) => {
-    return response.send("Login here.");
+    return response.send({message: "Login here."});
 }
 
 app.get("/", handleHome);
