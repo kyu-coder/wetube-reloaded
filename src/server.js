a29b4743358dd7492751ff9556fbae5973d3d027
kyu <kyu.fastai@gmail.com>
2021-04-28 14:37:11 -0500

    morgan

--- src/server.js
+++ src/server.js
@@ -1,13 +1,10 @@
 import express, { response } from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
-
-const logger = (request, response, next) => {
-    console.log(`${request.method} ${request.url}`);
-    next();
-}
+const logger = morgan("dev");
 
 const handleHome = (request, response) => {
     return response.send("I love middlewares");
@@ -17,24 +14,10 @@ const handleLogin = (request, response) => {
     return response.send({message: "Login here."});
 }
 
-const privateMiddleware = (request, response, next) => {
-    const url = request.url;
-    if(url === "/protected"){
-        return response.send("<h1>Not Allowed</h1>")
-    }
-    console.log("Allowed, you may continue");
-    next();
-}
-
-const handleProtected = (request, response) => {
-    return response.send("Welcome to the private lounge.");
-}
 
 app.use(logger);
-app.use(privateMiddleware);
 app.get("/", handleHome);
 app.get("/login", handleLogin);
-app.get("/protected", handleProtected);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🚀`);
 
