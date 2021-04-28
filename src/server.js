6e2907df7b3f65c278d684d7ffcb78899a63af1a
kyu <kyu.fastai@gmail.com>
2021-04-28 09:00:12 -0500

    middleware

--- src/server.js
+++ src/server.js
@@ -1,20 +1,40 @@
-import express from "express";
+import express, { response } from "express";
 
 const PORT = 4000;
 
 const app = express();
 
+const logger = (request, response, next) => {
+    console.log(`${request.method} ${request.url}`);
+    next();
+}
+
 const handleHome = (request, response) => {
-    // return response.end(); // ending the request (one of many ways to end)
-    return response.send("<h1> I still love you. </h1>"); // another way to end the request (with a sweet msg)
+    return response.send("I love middlewares");
 }
 
 const handleLogin = (request, response) => {
     return response.send({message: "Login here."});
 }
 
+const privateMiddleware = (request, response, next) => {
+    const url = request.url;
+    if(url === "/protected"){
+        return response.send("<h1>Not Allowed</h1>")
+    }
+    console.log("Allowed, you may continue");
+    next();
+}
+
+const handleProtected = (request, response) => {
+    return response.send("Welcome to the private lounge.");
+}
+
+app.use(logger);
+app.use(privateMiddleware);
 app.get("/", handleHome);
 app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🚀`);
 
