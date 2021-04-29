ec0446f8bfa8082b7ea729f6a4e75f5e51135029
kyu <kyu.fastai@gmail.com>
2021-04-29 02:32:17 -0500

    partials

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,7 @@
 export const trending = (req, res) => res.render("home");
-export const see = (req, res) => {
-    console.log(req.params);
-    res.send(`Watch Video #${req.params.id}`);
-}
-export const edit = (req, res) => {
-    res.send("Edit");
-}
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
+
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
