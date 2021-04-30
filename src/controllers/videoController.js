e4159df313622838fe13a53401ed256a44501794
kyu <kyu.fastai@gmail.com>
2021-04-29 21:39:13 -0500

    mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,31 @@
 
 export const trending = (req, res) => {
-    const videos = [];
+    const videos = [
+        {
+            title: "First Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },        
+        {
+            title: "Second Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+        {
+            title: "Third Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+    ];
     res.render("home", { pageTitle: "Home", videos })
 };
 export const see = (req, res) => res.render("watch");
