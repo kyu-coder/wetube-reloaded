defe6a3dcd1ebbade54ee18ef6c9034f10d8d4fe
kyu <kyu.fastai@gmail.com>
2021-04-29 21:24:31 -0500

    iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,8 @@
 
-const fakeUser = {
-    username:"Kyu",
-    loggedIn:false
+export const trending = (req, res) => {
+    const videos = [];
+    res.render("home", { pageTitle: "Home", videos })
 };
-
-export const trending = (req, res) => res.render("home", { pageTitle: "Home", fakeUser: fakeUser });
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 
