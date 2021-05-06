55202a88699a00596ce0beceac3da59366194d6a
kyu <kyu.fastai@gmail.com>
2021-05-06 16:15:16 -0500

    edit video part 1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,18 +4,24 @@ import Video from "../models/Video";
 
 export const home = async(req, res) => {
     const videos = await Video.find({});
-    console.log(videos);
     res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = async (req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id);
-    console.log(video);
+    if(!video){
+        return res.render("404", {pageTitle: "Video not found."});
+    }
     return res.render("watch", { pageTitle: video.title, video});
 };
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
     const { id } = req.params;
-    return res.render("edit", { pageTitle: `Editing`});
+    const video = await Video.findById(id);
+    if(!video){
+        return res.render("404", { pageTitle: "Video not found." });
+    }
+    return res.render("edit", { pageTitle: `Edit ${video.title}`, video });
+    
 };
 export const postEdit = (req, res) => {
     const { id } = req.params;
