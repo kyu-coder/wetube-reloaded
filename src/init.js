3c2fc2347f5b1a8801472e5e03630272d8fcce0b
kyu <kyu.fastai@gmail.com>
2021-05-10 01:25:24 -0500

    static

--- src/init.js
+++ src/init.js
@@ -3,7 +3,7 @@ import "./models/Video";
 import app from "./server"
 
 
-const PORT = 4001;
+const PORT = 4000;
 
 const handleListening = () => console.log(`✔ Server listening on port http://localhost:${PORT} 🚀`);
 
