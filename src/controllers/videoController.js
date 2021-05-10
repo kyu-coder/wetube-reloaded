1b19e8ce01a842ac5fba9f6cd2288ae87817c08c
kyu <kyu.fastai@gmail.com>
2021-05-10 01:33:08 -0500

    delete videos

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -59,3 +59,10 @@ export const postUpload = async (req, res) => {
         });
     }
 }
+
+export const deleteVideo = async(req, res) => {
+    const { id } = req.params;
+    await Video.findByIdAndDelete(id);
+    
+    return res.redirect("/");
+}
