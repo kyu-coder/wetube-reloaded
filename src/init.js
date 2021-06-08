85baefb4dd42b980614dff6eae93a22f40213a02
kyu <kyu.fastai@gmail.com>
2021-06-08 03:20:05 -0500

    hash password

--- src/init.js
+++ src/init.js
@@ -4,7 +4,7 @@ import "./models/User";
 import app from "./server";
 
 
-const PORT = 4000;
+const PORT = 4001;
 
 const handleListening = () => console.log(`✔ Server listening on port http://localhost:${PORT} 🚀`);
 
