13f799939d4c8b1097c0855a092e6e30cc366f2e
kyu <kyu.fastai@gmail.com>
2021-06-09 02:24:49 -0500

    login logout profile sessions etc

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -55,7 +55,9 @@ export const postLogin = async(req, res) => {
         });
     }
 
-    console.log("LOG USER IN! COMING SOON!");
+    req.session.loggedIn = true;
+    req.session.user = user;
+    
     return res.redirect("/");
 }
 
